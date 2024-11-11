3129036d5a54c524003191205a19b5059c4e879b
Pyae Sone Win <pyaesonewin.dev@gmail.com>
2024-11-11 10:22:41 +0700

    fix: update adjustPercent to preprocess input for better validation in issuerSchema

--- src/schemas/issuerSchema.ts
+++ src/schemas/issuerSchema.ts
@@ -5,7 +5,7 @@ const issuerSchema = z.object({
         message: 'Name must be at least 3 characters long',
     }),
     nonEmvTranRequirePIN: z.boolean(),
-    adjustPercent: z.number(),
+    adjustPercent: z.preprocess((val: unknown) => parseInt(val as string, 10), z.number().optional()),
     isAllowManualPan: z.boolean(),
     panMaskPattern: z.string().min(3, {
         message: 'Pan Mask Pattern must be at least 3 characters long',

f5734507b8e377035de52ff6330f72313113f640
Pyae Sone Win <pyaesonewin.dev@gmail.com>
2024-12-12 16:19:15 +0700

    feat: add environment variable logging in CalculationEngine component

--- src/app/(authenticated)/payout/page.tsx
+++ src/app/(authenticated)/payout/page.tsx
@@ -1,6 +1,8 @@
 import { PayoutForm } from "@/components/form/PayoutForm";
 
 export default function CalculationEngine() {
+    console.log("Environment: ", process.env.NEXT_PUBLIC_BASE_URL);
+    console.log("Environment: ", process.env.NEXT_PAYOUT_API_URL);
     return (
         <>
             <PayoutForm />

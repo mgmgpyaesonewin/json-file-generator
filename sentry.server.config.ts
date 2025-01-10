80ddfb04beca652d668ee231e95c08234dbc347a
Pyae Sone Win <pyaesonewin.dev@gmail.com>
2025-01-10 16:18:59 +0700

    Initial Sentry Setup for Production Env

---
+++ sentry.server.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  enabled: process.env.NODE_ENV === "production",
+  dsn: "https://fb8c0661adb16a8b0f81ece7dd23ecef@o4508617567633408.ingest.us.sentry.io/4508617579560960",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});

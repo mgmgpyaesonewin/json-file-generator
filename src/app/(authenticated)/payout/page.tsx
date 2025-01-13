737ecb8ca248bdbb680bb45ae54cd6470634d6c2
Pyae Sone Win <pyaesonewin.dev@gmail.com>
2025-01-13 12:08:18 +0700

    feat: remove console log

--- src/app/(authenticated)/payout/page.tsx
+++ src/app/(authenticated)/payout/page.tsx
@@ -1,8 +1,6 @@
 import { PayoutForm } from "@/components/form/PayoutForm";
 
 export default function CalculationEngine() {
-    console.log("Environment: ", process.env.NEXT_PUBLIC_BASE_URL);
-    console.log("Environment: ", process.env.NEXT_PAYOUT_API_URL);
     return (
         <>
             <PayoutForm />
